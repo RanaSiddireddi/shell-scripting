131f6ed80a4941366bcc7a1ba436996cb92c28e1
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-01-23 22:03:19 +1100

    added 08

--- 08-function.sh
+++ 08-function.sh
@@ -5,3 +5,13 @@ sample() {
 }
 
 sample 
+
+stat(){
+    echo "number of sessions opened : $(who | wc -1)"
+    echo "todays date is : $(date +%F)"
+    echo "load average of the system in last 1 minute: $(uptime | awk -F : '{print $5}' | awk -F , '{print $1}')"
+}
+
+echo "calling stat function"
+
+stat
