9e066e4578fbfcd807929c230c948d3dfcb00b15
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-01-24 22:52:22 +1100

    automation

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+set -e
+
 COMPONENT=frontend
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[31m you need to be root user or with sudo priviledge \e[0m"
+    exit 1
+fi
 
 stat() {
     if [ $1 -eq 0 ] ; then
@@ -10,40 +19,33 @@ stat() {
     fi
 }
 
-ID=$(id -u)
-
-if [ $ID -ne 0 ] ; then
-    echo -e "\e[31m you need to be root user or with sudo priviledge \e[0m"
-    exit 1
-fi
-
 echo -n "installing nginx :"
-yum install nginx -y    &>> /tmp/frontend.log
+yum install nginx -y    &>> /tmp/$COMPONENT.log
 stat $?
 
 echo -n "downloading the $COMPONENT :"
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $?
 
 echo -n "clearing default content :"
 cd /usr/share/nginx/html
-rm -rf *    &>> /tmp/frontend.log
+rm -rf *    &>> /tmp/$COMPONENT.log
 stat $?
 
 echo -n "extracting $COMPONENT :"
-unzip /tmp/$COMPONENT.zip   &>> /tmp/frontend.log
+unzip /tmp/$COMPONENT.zip   &>> /tmp/$COMPONENT.log
 stat $?
 
 echo -n "copying the content : "
-mv frontend-main/* .    &>> /tmp/frontend.log
-mv static/* .   &>> /tmp/frontend.log
-rm -rf frontend-main README.md  &>> /tmp/frontend.log
-mv localhost.conf /etc/nginx/default.d/roboshop.conf    &>> /tmp/frontend.log
+mv frontend-main/* .    &>> /tmp/$COMPONENT.log
+mv static/* .   &>> /tmp/$COMPONENT.log
+rm -rf frontend-main README.md  &>> /tmp/$COMPONENT.log
+mv localhost.conf /etc/nginx/default.d/roboshop.conf    &>> /tmp/$COMPONENT.log
 stat $?
 
 echo -n "starting nginx :"
-systemctl enable nginx  &>> /tmp/frontend.log
-systemctl restart nginx   &>> /tmp/frontend.log
+systemctl enable nginx  &>> /tmp/$COMPONENT.log
+systemctl restart nginx   &>> /tmp/$COMPONENT.log
 stat $?
 
 
