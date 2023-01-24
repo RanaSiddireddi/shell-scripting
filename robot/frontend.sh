b0aa25cecd6daf737161844c1f1ea4f6b7b7a9c9
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-01-24 22:13:30 +1100

    automation

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -7,6 +7,9 @@ if [ $ID -ne 0 ] ; then
     exit 1
 fi
 
+echo "installing nginx :"
 yum install nginx -y    &>> /tmp/frontend.log
 systemctl enable nginx  &>> /tmp/frontend.log
+
+echo "starting nginx :"
 systemctl start nginx   &>> /tmp/frontend.log
