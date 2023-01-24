edac750fec84546fdc2bba88a11ffd194865f691
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-01-24 22:05:35 +1100

    automation

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -2,11 +2,11 @@
 
 ID=$(id -u)
 
-if [ $UID -ne 0 ] ; then
+if [ $ID -ne 0 ] ; then
     echo -e "\e[31m you need to be root user or with sudo priviledge \e[0m"
     exit 1
 fi
 
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
+yum install nginx -y    &> /tmp/frontend.log
+systemctl enable nginx  &> /tmp/frontend.log
+systemctl start nginx   &> /tmp/frontend.log
