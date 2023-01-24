5abf877f2ffbbc97c06ae005d587424b37c750b1
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-01-24 22:22:14 +1100

    automation

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -9,7 +9,17 @@ fi
 
 echo "installing nginx :"
 yum install nginx -y    &>> /tmp/frontend.log
+if [ $? -eq 0] ; then
+    echo -e "\e[32m success \e[0m"
+else
+    echo -e "\e[31m failed \e[0m"
+fi
 systemctl enable nginx  &>> /tmp/frontend.log
 
 echo "starting nginx :"
 systemctl start nginx   &>> /tmp/frontend.log
+if [ $? -eq 0] ; then
+    echo -e "\e[32m success \e[0m"
+else
+    echo -e "\e[31m failed \e[0m"
+fi
