704341965d74e299efe25af1647fbe4845745fd7
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-01-24 21:52:33 +1100

    automation

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -3,7 +3,7 @@
 UID=$(id -u)
 
 if [ $UID -ne 0 ] ; then
-    echo -e "\e[32m you need to be root user or with sudo priviledge \e[0m"
+    echo -e "\e[31m you need to be root user or with sudo priviledge \e[0m"
     exit 1
 fi
 
