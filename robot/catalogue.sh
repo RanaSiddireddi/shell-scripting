fe0d7be1ee7eeef2ecf639b56307e8caa15c172a
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-01-26 14:44:33 +1100

    automation mongodb

--- robot/catalogue.sh
+++ robot/catalogue.sh
@@ -2,7 +2,7 @@
 
 COMPONENT=catalogue
 
-source common.sh
+source ./common.sh
 
 echo -n "configuring and installing nodejs repo : "
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
--- robot/common.sh
+++ robot/common.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 LOGFILE=/tmp/$COMPONENT.log
 
 ID=$(id -u)
