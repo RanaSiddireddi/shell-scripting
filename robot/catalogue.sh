c5d10f892f9b7e499b90a4d3bc59cf4c9519638a
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-01-26 18:14:33 +1100

    automation mongodb

--- robot/catalogue.sh
+++ robot/catalogue.sh
@@ -5,7 +5,7 @@ COMPONENT=catalogue
 source "robot/common.sh"
 
 echo -n "configuring and installing nodejs repo : "
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -    &>> "$LOGFILE"
 yum install nodejs -y &>> "$LOGFILE"
 stat $?
 
