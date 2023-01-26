c76c8e97dd7478068898b1d6313b5c2de7016d0b
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-01-26 14:45:04 +1100

    automation mongodb

--- robot/catalogue.sh
+++ robot/catalogue.sh
@@ -6,9 +6,8 @@ source ./common.sh
 
 echo -n "configuring and installing nodejs repo : "
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
-yum install nodejs -y   &>> "$LOGFILE"
+yum install nodejs -y   
 stat $?
 
-echo -n "creating application user $APPUSER : "
-useradd roboshop    &>> "$LOGFILE"
-stat $?
+# echo -n "creating application user $APPUSER : "
+# useradd roboshop   
