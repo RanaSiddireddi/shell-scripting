b3aa43901daab339b776842a9e61aa6311d47fa1
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-01-28 18:22:58 +1100

    automation user

--- robot/mysql.sh
+++ robot/mysql.sh
@@ -21,9 +21,29 @@ DEFAULT_ROOT_PWD=$(sudo grep 'A temporary password' /var/log/mysqld.log | awk -F
 stat $?
 
 
-echo show databases | mysql -uroot -pRoboShop@1 &>> "$LOGFILE"
+echo "show databases" | mysql -uroot -pRoboShop@1 &>> "$LOGFILE"
 if [ $? -ne 0 ] ; then
     echo -n "resetting default root password :"
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1' ;" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PWD}   &>> "$LOGFILE"
     stat $?
 fi
+ 
+echo "show plugins" | mysql -uroot -pRoboShop | grep validate_password; &>> "$LOGFILE"
+if [ $? -ne 0 ] ; then
+    echo -n "uninstalling validate passowrd plugin :"
+    echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1 &>> "$LOGFILE"
+    stat $?
+fi
+
+echo -n "downloading schema $COMPONENT :"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"  &>> "$LOGFILE"
+cd /tmp
+unzip $COMPONENT.zip &>> "$LOGFILE"
+stat $?
+
+echo -n "injecting $COMPONENT schema :"
+cd $COMPONENT-main
+mysql -uroot -pRoboShop@1 <shipping.sql
+stat $?
+
+echo -e "\e[32m___________$COMPONENT Configuration complete_____________ \e[0m"
