439dfc6179156928f20c307cb6fb5dc0d72ec2bd
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-01-31 00:50:12 +1100

    automation rabitmq WIP

---
+++ robot/launch-ec2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COMPONENT = $1
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=b52-ansible-dev-20Jan2023" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52-allow-all --region us-east-1 |jq .SecurityGroups[].GroupId | sed -e 's/"//g')
+
+echo "AMI ID to launch the instance is $AMI_ID "
+echo "security group id used to launch instance is $SG_ID"
+echo "****____$COMPONENT launch is in progress____****"
+
