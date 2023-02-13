0889acb8524c0a9f9d4aed992e4419b1db7fd712
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-02-13 18:52:47 +1100

    automation rabitmq WIP

--- robot/launch-ec2.sh
+++ robot/launch-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMPONENT = $1
+COMPONENT=$1
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=b52-ansible-dev-20Jan2023" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52-allow-all --region us-east-1 |jq .SecurityGroups[].GroupId | sed -e 's/"//g')
@@ -9,3 +9,4 @@ echo "AMI ID to launch the instance is $AMI_ID "
 echo "security group id used to launch instance is $SG_ID"
 echo "****____$COMPONENT launch is in progress____****"
 
+# aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --security-group-ids ${SG_ID} --tags key=Name,value=${COMPONENT}
