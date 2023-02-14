e7cb5d02c3994f632b2f1edc101c7e4c2edb9469
RanaSiddireddi <rana.kaushik06@gmail.com>
2023-02-14 17:34:11 +1100

    automation rabitmq WIP

--- robot/launch-ec2.sh
+++ robot/launch-ec2.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [ -z "$COMPONENT" ]; then
+    echo -e "\e[31m component name is required \n sample usage: \n\n\t\t bash launch-ec2.sh componentname \e[0m "
+    exit 1
+fi
+
+
 COMPONENT=$1
+ENV=$2
 HOSTED_ZONE_ID="Z08504361Q1M69BII5VS1"
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=b52-ansible-dev-20Jan2023" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
@@ -8,14 +15,29 @@ SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52-al
 
 echo "AMI ID to launch the instance is $AMI_ID "
 echo "security group id used to launch instance is $SG_ID"
-echo "****____$COMPONENT launch is in progress____****"
-
-PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
 
 
-echo -n "Creating Internal DNS record for $COMPONENT"
+launch_ec2() {
+    echo "****____$COMPONENT launch is in progress____****"
+    PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-$ENV}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
 
-sed -e "s/IPADDRESS/$PRIVATE_IP/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/r53.json
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch file:///tmp/r53.json
+    echo -n "Creating Internal DNS record for $COMPONENT-$ENV"
 
-echo -n "****____DNS record for $COMPONENT is complete____****"
+    sed -e "s/IPADDRESS/$PRIVATE_IP/" -e "s/COMPONENT/${COMPONENT}-${ENV}/" route53.json > /tmp/r53.json
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch file:///tmp/r53.json
+
+    echo -n "****____DNS record for $COMPONENT-$ENV is complete____****"
+}
+
+
+if ["$1" == "all"]; then
+    for component in frontend catalogue cart user shipping payment mysql rabbitmq redis mongodb; do
+        COMPONENT=$component
+        launch_ec2
+    done
+else 
+        launch_ec2
+fi
+
+
+
